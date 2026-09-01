5590e07a5e2fe4318753a075829896914121f14a
overwr <seeklklk32@proton.me>
2026-09-01 13:13:00 +0300

    Update CS.py for enhanced file transfer functionality
    Refactor file transfer protocol with improved flags and buffer handling.

--- CS.py
+++ CS.py
@@ -4,17 +4,24 @@ from itertools import islice
 import sys
 import time
 
+PORT = 39871
+BUFFER = 1024
+EOF_Flag = b'---EOF---EOF---FCSFLAG---...---EOF---EOF---FCSFLAG---'
+CONT_Flag = b'CONTINUE---...000--...FCGFLAG'
+
+def Wait(Socket):
+    return Socket.recv(BUFFER)
+
+def Continue(Socket):
+    Socket.send(b'wait')
+
+
 def Find():
     #Pattern = 'C:/**/*.txt'
-    Pattern = '*.jpg'
-    return list(islice(glob.iglob(Pattern, recursive=True), 4))
+    Pattern = './*.jpg'
+    #return list(islice(glob.iglob(Pattern, recursive=True), 3))
+    return list(glob.iglob(Pattern, recursive=True))
 
-def SendFile(sock, filepath):
-    f = open(filepath, 'rb')
-    datas = 1
-    while datas:
-        datas = f.read(1024)
-        sock.send(datas)
 
 def GetFilename(Msg):
     new = Msg.split('/')
@@ -27,31 +34,12 @@ def TryToDecode(msg):
     except:
         return -1
 
-def CreateDownl(Socket, Filename):
-    print("file--- " + Filename)
-    print('aa')
-    f = open(Filename, 'wb+')
-    while True:
-        datas = 1
-        while datas:
-            datas = Socket.recv(2048)
-            eof = "---EOF---EOF---FCSFLAG---"
-            out = TryToDecode(datas)
-            if out != -1:
-                print(datas)
-                if eof in TryToDecode(datas):
-                    break
-
-            f.write(datas)
-        f.close()
-        break
-    print("Downloaded...")
 
 
 def Receiver():
     Socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 
-    Socket.bind(("", 9050))
+    Socket.bind(("", PORT))
 
     Socket.listen(3)
 
@@ -64,16 +52,26 @@ def Receiver():
         msg = ClientSocket.recv(256).decode() # filename
         if msg == '':
             break
-        CreateDownl(ClientSocket, msg)
-        print("before 1")
-        ClientSocket.send('1'.encode())
+        Continue(ClientSocket)
+        print("file--- " + msg)
+        f = open(msg, 'wb+')
+        while True:
+            flag = Wait(ClientSocket)
+            if flag == CONT_Flag:
+                Continue(ClientSocket)
+                f.write(ClientSocket.recv(BUFFER))
+            else:
+                f.close()
+                break
+        print("Downloaded...")
+        Continue(ClientSocket)
 
 
 def Sender():
     Socket = s.socket(s.AF_INET, s.SOCK_STREAM)
     while True:
         try:
-            Socket.connect(("127.0.0.1", 9050))
+            Socket.connect(("127.0.0.1", PORT))
         except:
             continue
         break
@@ -84,13 +82,21 @@ def Sender():
         print(list)
         name = GetFilename(list[ll])
         Socket.send(name.encode())
-        SendFile(Socket, list[ll])
-        time.sleep(2)
-        Socket.send("---EOF---EOF---FCSFLAG---".encode())
+        Wait(Socket)
+        f = open(list[ll], 'rb')
+        while True:
+            datas = f.read(BUFFER)
+            if datas == b'':
+                Socket.send(EOF_Flag)
+                break
+            else:
+                Socket.send(CONT_Flag)
+                Wait(Socket)
+                Socket.send(datas)
         list.remove(name)
         print(list)
         print("file done")
-        Socket.recv(16)
+        Wait(Socket)
         print("\n\n"+str(ll))
         ll-=1
 
